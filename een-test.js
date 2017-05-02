c2839a6a44b7404e2230393cd33621923a56b14a
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-02 20:34:00 +0800

    Updated test.

--- een-test.js
+++ een-test.js
@@ -1,7 +1,10 @@
 "use strict";
 
+const assert = require( "assert" );
 const een = require( "./een.js" );
 
-console.log( een( [ 1, 2, 3 ], 2 ) );
-console.log( een( [ 1, 2, 3 ], 4 ) );
-console.log( een( [ null, undefined ], "" ) );
+assert.equal( een( [ 1, 2, 3 ], 2 ), true, "should be true" );
+assert.equal( een( [ 1, 2, 3 ], 4 ), false, "should be false" );
+assert.equal( een( [ null, undefined ], "" ), false, "should be false" );
+
+console.log( "ok" );
