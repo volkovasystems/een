0c0a03079c38682e0d21027ca439b2ebf8d5977a
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-07 16:57:08 +0800

    Updated server/client test.

--- een-test.js
+++ een-test.js
@@ -4,12 +4,11 @@ const een = require( "./een.js" );
 
 assert.equal( een( [ 1, 2, 3 ], 2 ), true, "should be true" );
 
+assert.equal( !een( [ 1, 2, 3 ], 4 ), true, "should be true" );
+
+assert.equal( !een( [ 1, 2, 3, [ 4 ] ], [ 4 ] ), true, "should be true" );
+
 assert.equal( een( [ ] ), false, "should be false" );
 
-assert.equal( een( [ 1, { } ], { } ), false, "should be false" );
-
-assert.equal( een( [ 1, 2, 3, [ 4 ] ], [ 4 ] ), false, "should be false" );
-
-assert.equal( een( [ 1, 2, 3 ], 4 ), false, "should be false" );
 
 console.log( "ok" );
