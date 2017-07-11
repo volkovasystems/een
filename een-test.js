b93d37330f2199570f500f7ae51207adca97ae3f
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-11 12:16:07 +0800

    Updated server-side test.

--- een-test.js
+++ een-test.js
@@ -1,6 +1,7 @@
 
 const assert = require( "assert" );
 const een = require( "./een.js" );
+const eqe = require( "eqe" );
 
 assert.equal( een( [ 1, 2, 3 ], 2 ), true, "should be true" );
 
@@ -8,7 +9,16 @@ assert.equal( !een( [ 1, 2, 3 ], 4 ), true, "should be true" );
 
 assert.equal( !een( [ 1, 2, 3, [ 4 ] ], [ 4 ] ), true, "should be true" );
 
-assert.equal( een( [ ] ), false, "should be false" );
+let source = [ 1, 2, 3, 7 ];
+let target = [ 1, 2, 3, 4, 5, 6 ];
 
+source.forEach( ( value ) => {
+	!een( target, value, ( element, value ) => eqe( element, value ) ) &&
+	target.push( value );
+} );
+
+assert.deepEqual( target, [ 1, 2, 3, 4, 5, 6, 7 ], "should be equal" );
+
+assert.equal( een( [ ] ), false, "should be false" );
 
 console.log( "ok" );
